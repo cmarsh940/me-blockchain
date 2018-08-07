73ebbc30cc38e7adee92325fd0fbf8a0ecca7f1f
Cam <cam.marshall940@yahoo.com>
2018-08-07 14:11:34 -0500

    updated

--- apps/surveysbyme-apps/src/app/client/client.module.ts
+++ apps/surveysbyme-apps/src/app/client/client.module.ts
@@ -1,4 +1,17 @@
-import { PersonComponent } from './person/person.component';
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { addSurveyComponent } from './add-survey/add-survey.component';
 import { ClientComponent } from './client.component';
 import { NgModule } from '@angular/core';
@@ -7,9 +20,9 @@ import { CommonModule } from '@angular/common';
 import { ClientRoutingModule } from './client-routing.module';
 import { ClientNavComponent } from './client-nav/client-nav.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { SurveyComponent } from './Survey/Survey.component';
-
-import { CustomerComponent } from './Customer/Customer.component';
+import { SurveyComponent } from './survey/survey.component';
+import { PersonComponent } from "./person/person.component";
+import { CustomerComponent } from './customer/customer.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProfileComponent } from './profile/profile.component';
 import { SurveyCategoryComponent } from './survey-category/survey-category.component';
